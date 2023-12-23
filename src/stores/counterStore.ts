e09497b8e1536bd237e6d412634fb7fa10ff1220
lonzzi <lonzzi@qq.com>
2023-12-23 20:21:37 +0800

    fix: misspell word

--- src/stores/counterStore.ts
+++ src/stores/counterStore.ts
@@ -1,7 +1,7 @@
 import { defineStore } from "pinia"
 import { ref } from "vue"
 
-export const useCouterStore = defineStore("counter", () => {
+export const useCounterStore = defineStore("counter", () => {
   const count = ref(0)
   const increment = () => count.value++
   return {
