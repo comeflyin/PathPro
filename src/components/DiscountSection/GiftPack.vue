5e4c23cf928224e2ffd69d295fc9f8b4c5387530
comeflyin <1252253192@qq.com>
2024-01-05 01:13:06 +0800

    bgChange

--- src/components/DiscountSection/GiftPack.vue
+++ src/components/DiscountSection/GiftPack.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="m-2 mt-5">
-    <div class="m-2 mt-7 border rounded-3xl relative bg-[#fee8bf] h-auto">
+    <div class="m-2 mt-7 border rounded-3xl relative bg-[#ffe0ce] h-auto">
       <img
         src="https://res.klook.com/image/upload/fl_lossy.progressive,w_208,h_208,c_fill,q_85/v1677035412/UED_new/Platform/platform_deals_spot_illustration_2302/sales/value_pack.webp"
         alt=""
