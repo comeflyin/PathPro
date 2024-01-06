664857db579af031bd4244d6f38f89085675014b
lx779940 <3122178991@qq.com>
2024-01-06 23:18:07 +0800

    我的订单

---
+++ src/types/Order.ts
@@ -0,0 +1,12 @@
+export type Order = {
+  id: number
+  name: string
+  img: string
+  direction: string
+  type: string
+  additon: string
+  human?: number
+  childen?: number
+  old?: number
+  price: number
+}
--- src/views/TravelPlan/MyOrder.vue
+++ src/views/TravelPlan/MyOrder.vue
@@ -1,14 +1,50 @@
 <template>
-  <div v-if="order.length > 0">我的订单</div>
-  <div v-if="order.length == 0">
-    <img src="../../assets/image/NoWait.png" />
+  <!-- 我的订单页 -->
+  <div v-if="order.order">
+    <div v-for="item in order.order" :key="item.id">
+      <MyBuyOrder
+        :img="item.img"
+        :name="item.name"
+        :direction="item.direction"
+        :type="item.type"
+        :additon="item.additon"
+        :human="item.human"
+        :childen="item.childen"
+        :old="item.old"
+        :price="item.price"
+      />
+    </div>
+  </div>
+  <div v-else>
+    <img src="../../assets/image/NoOrder.jpg" />
+  </div>
+  <div class="m-2 font-bold">热门活动</div>
+  <div class="overflow-x-scroll scroll-auto scroll-mr-2 w-full rounded-lg flex">
+    <div v-for="Activities in Activity.popularActivities" :key="Activities.id">
+      <PopularActivity
+        :img="Activities.img"
+        :name="Activities.name"
+        :star="Activities.star"
+        :comment="Activities.comment"
+        :oldPrice="Activities.oldPrice"
+        :price="Activities.price"
+      />
+    </div>
+  </div>
+  <div class="text-center border-black border-2 mt-3 rounded-lg">
+    <button>查看更多</button>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue"
+import { usePopularActivityStore } from "@/stores/PopularActivitiesStore"
+import { useOrderStore } from "@/stores/OrderStore"
+import PopularActivity from "@/components/TravelAgent/PopularActivity.vue"
+import MyBuyOrder from "@/components/TravelAgent/MyBuyOrder.vue"
 
-const order = ref([])
+const Activity = usePopularActivityStore()
+const order = useOrderStore()
+console.log(order.order)
 </script>
 
 <style scoped></style>
