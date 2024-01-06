30a65b173441c3a9aa136501aa679cceb88bcbaf
lx779940 <3122178991@qq.com>
2024-01-06 23:52:43 +0800

    add generics

--- src/components/TravelAgent/MyBuyOrder.vue
+++ src/components/TravelAgent/MyBuyOrder.vue
@@ -1,33 +1,26 @@
 <template>
   <div class="flex align-start justify-start mt-4">
     <div class="w-20 h-20 rounded-xl overflow-hidden">
-      <img :src="img" alt="" />
+      <img :src="item.img" alt="" />
     </div>
     <div class="flex-1 ml-2">
-      <div class="text-xl font-bold">{{ name }}</div>
-      <div class="text-sm">{{ direction }}·{{ type }}·{{ additon }}</div>
+      <div class="text-xl font-bold">{{ item.name }}</div>
+      <div class="text-sm">{{ item.direction }}·{{ item.type }}·{{ item.additon }}</div>
       <div>有效期内皆适用</div>
-      <span class="text-sm" v-if="human">{{ human }}x成人</span>
-      <span class="text-sm" v-if="childen">{{ childen }} x儿童(3-11岁) </span>
-      <span class="text-sm" v-if="old">{{ old }} x长者 香港居民(60岁及以上)</span>
-      <div class="text-xl font-bold">￥{{ price }}</div>
+      <span class="text-sm" v-if="item.human">{{ item.human }}x成人</span>
+      <span class="text-sm" v-if="item.childen">{{ item.childen }} x儿童(3-11岁) </span>
+      <span class="text-sm" v-if="item.old">{{ item.old }} x长者 香港居民(60岁及以上)</span>
+      <div class="text-xl font-bold">￥{{ item.price }}</div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import type { Order } from "@/types/Order"
 import { defineProps } from "vue"
-defineProps({
-  name: String,
-  img: String,
-  direction: String,
-  type: String,
-  additon: String,
-  human: Number,
-  childen: Number,
-  old: Number,
-  price: Number,
-})
+defineProps<{
+  item: Order
+}>()
 </script>
 
 <style scoped></style>
