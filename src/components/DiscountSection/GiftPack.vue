45405d88344a5b62e54aa0e959cd3ecd03ae03b1
comeflyin <1252253192@qq.com>
2024-01-07 01:01:46 +0800

    MsgItem

--- src/components/DiscountSection/GiftPack.vue
+++ src/components/DiscountSection/GiftPack.vue
@@ -11,39 +11,14 @@
       <span class="m-3 mt-0 text-sm/[22px] inline-block w-52">预定多个热门景点，畅享超值优惠</span>
       <!-- 礼卷 -->
       <div class="bg-white rounded-2xl p-4 h-full border-neutral-200 border">
-        <div class="flex rounded-xl mt-2 mb-2" v-for="(item, index) in giftsInfo" :key="index">
-          <!-- 封面 -->
-          <img :src="item.cover" alt="" class="w-2/5 h-auto rounded-xl" />
-          <!-- 右详情信息 -->
-          <div class="ml-3 flex flex-col justify-center w-1/2">
-            <!-- 城市信息 -->
-            <span class="text-sm text-[#7d7d7d] mt-1 flex-1">{{ item.city }}</span>
-            <!-- 礼卷信息标题 -->
-            <span class="flex-1 text-sm font-semibold w-11/12 whitespace-nowrap overflow-hidden text-ellipsis">
-              {{ item.title }}
-            </span>
-            <div class="text-sm flex-nowrap flex flex-1">
-              <!-- 参与人数 -->
-              <span class="flex-1 text-[#9a9a9a] w-3/4 whitespace-nowrap overflow-hidden text-ellipsis">
-                {{ item.participants }}+人参与
-              </span>
-            </div>
-            <div class="flex-1">
-              <!-- 价格 -->
-              <span class="text-[#ff5b00]">￥{{ item.price }}</span>
-            </div>
-            <div class="flex-1 overflew-hidden flex-nowrap whitespace-nowrap w-1/2">
-              <!-- 打折信息 -->
-              <span
-                class="text-[#ff5b00] bg-[#fff0e5] text-xs text-center font-semibold rounded-lg mr-2 pl-2 pr-2 pt-1 pb-1"
-                v-for="(discountMsg, index) in item.discounts"
-                :key="index"
-              >
-                {{ discountMsg }}
-              </span>
-            </div>
-          </div>
-        </div>
+        <MsgItem
+          v-for="(item, index) in giftsInfo"
+          :key="index"
+          :discount="item"
+          :isflex="true"
+          :width="'full'"
+          :widthimg="'2/5'"
+        />
       </div>
     </div>
   </div>
@@ -52,6 +27,7 @@
 <script setup lang="ts">
 import { useGiftStore } from "@/stores/discountStore"
 import { ref } from "vue"
+import MsgItem from "./MsgItem.vue"
 
 const gifts = useGiftStore()
 const giftsInfo = ref(gifts.giftsInfo)
