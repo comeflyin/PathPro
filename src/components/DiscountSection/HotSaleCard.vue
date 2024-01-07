8b75497e967dcecdc22ee949ab3425b02200b710
comeflyin <1252253192@qq.com>
2024-01-08 02:33:11 +0800

    DiscountHotSale

---
+++ src/components/DiscountSection/HotSaleCard.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="item-warpper w-full p-2 space-y-1" :ref="forwardRef">
+    <div class="img-wrapper rounded-xl overflow-hidden relative">
+      <img class="w-full" :src="item.cover" alt="" />
+      <div class="item-mask absolute w-full h-full bg-black opacity-20 bottom-0"></div>
+      <div class="area absolute bottom-1 left-1 text-white text-sm font-bold flex items-center space-x-1">
+        <van-icon name="location" />
+        <div class="area__text">{{ item.city }}</div>
+      </div>
+    </div>
+    <div class="title text-sm font-bold">{{ item.title }}</div>
+    <!-- 评分与参与情况 -->
+    <div class="text-sm flex-nowrap flex">
+      <span class="text-[#f09b0a]" v-if="item.rating"><van-icon name="star" />{{ item.rating }}</span>
+      <span class="text-[#9a9a9a] w-3/4 whitespace-nowrap overflow-hidden text-ellipsis">
+        <span>({{ item.evaluate }})</span>
+      </span>
+    </div>
+    <div class="price text-sm font-bold">
+      {{ getCurrency(item.price) }}
+      <span class="font-normal text-xs">起</span>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { HotSaleItem } from "@/types/hotSale"
+import { getCurrency } from "@/utils/utils"
+import { useForwardRef } from "@/hooks/useForwardRef"
+
+defineProps<{
+  item: HotSaleItem
+}>()
+
+const forwardRef = useForwardRef()
+</script>
+
+<style scoped></style>
---
+++ src/components/DiscountSection/HotSaleList.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="list-wrapper flex flex-wrap items-center">
+    <div class="item flex-[0_0_50%]" v-for="(item, index) in items" :key="index">
+      <HotSaleCard :item="item" :ref="(el) => (index === items.length - 1 ? (itemRef = el as HTMLElement) : '')" />
+    </div>
+  </div>
+  <div class="text-center" v-if="!hasMore">没有更多了</div>
+</template>
+
+<script setup lang="ts">
+import type { HotSaleItem } from "@/types/hotSale"
+import HotSaleCard from "./HotSaleCard.vue"
+import useIntersectionObserver from "@/hooks/useIntersectionObserver"
+import { ref } from "vue"
+
+defineOptions({
+  inheritAttrs: false,
+})
+
+const itemRef = ref<HTMLElement | null>(null)
+
+const props = defineProps<{
+  items: HotSaleItem[]
+  /** 获取下一页的方法 */
+  next?: (setHasMore: (value: boolean) => void) => void
+}>()
+
+// itemRef 最后一个元素的引用
+const { hasMore, setHasMore } = useIntersectionObserver(itemRef, () => {
+  // 当最后一个元素出现在视口时，触发回调函数
+  // 调用父组件传递的 next 方法，加载下一页
+  props.next?.(setHasMore)
+})
+</script>
+
+<style scoped></style>
