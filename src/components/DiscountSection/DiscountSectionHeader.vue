82ba3a5b652026bfdfe652ec2b8f0c48e5302f2e
comeflyin <1252253192@qq.com>
2024-01-08 19:07:02 +0800

    StyleChange

--- src/components/DiscountSection/DiscountSectionHeader.vue
+++ src/components/DiscountSection/DiscountSectionHeader.vue
@@ -5,7 +5,7 @@
       专属推荐
       <van-icon name="arrow-down" />
     </button>
-    <div class="absolute right-4 top-1 text-slate-700">
+    <div class="absolute right-4 text-slate-700">
       <van-icon name="share-o" size="25" />
     </div>
   </div>
