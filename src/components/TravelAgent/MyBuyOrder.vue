a511fbd4e54c0ba2825a70a2f590811212c39378
lx779940 <3122178991@qq.com>
2024-01-12 16:33:03 +0800

    MyBuyOrder

--- src/components/TravelAgent/MyBuyOrder.vue
+++ src/components/TravelAgent/MyBuyOrder.vue
@@ -10,14 +10,14 @@
       <span class="text-sm" v-if="item.human">{{ item.human }}x成人</span>
       <span class="text-sm" v-if="item.childen">{{ item.childen }} x儿童(3-11岁) </span>
       <span class="text-sm" v-if="item.old">{{ item.old }} x长者 香港居民(60岁及以上)</span>
-      <div class="text-xl font-bold">￥{{ item.price }}</div>
+      <div class="text-xl font-bold">{{ getCurrency(item.price) }}</div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { getCurrency } from "@/utils/utils"
 import type { Order } from "@/types/Order"
-import { defineProps } from "vue"
 defineProps<{
   item: Order
 }>()
